0f9d92d283f1bf2f5ce9f3f98eabaae4bf85ac1f
Alaa Mohammed Gamal Eldin <alaagamalel-din@hotmail.com>
2024-03-24 17:35:40 +0200

    change the class to interface

--- src/Model/user.ts
+++ src/Model/user.ts
@@ -1,16 +1,5 @@
 import { Visibility } from "src/app/enums/friendsVidibility.enum";
-export class User{
-    constructor(userId: string, username: string, userEmail: string, firstName: string, middleName: string, lastName: string, userTitle: string, userAvatarUrl: string, friendsVisibility: Visibility){
-        this.userId = userId;
-        this.username = username;
-        this.userEmail = userEmail;
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.userTitle = userTitle;
-        this.userAvatarUrl = userAvatarUrl;
-        this.friendsVisibility = friendsVisibility;
-    }
+export interface User{
     userId: string;
     username: string;
     userEmail: string;
@@ -20,5 +9,4 @@ export class User{
     userTitle: string;
     userAvatarUrl: string;
     friendsVisibility: Visibility;
-   
 }
