40bc477b8a91cdcd23872f891b2bd8b3f30e8629
Alaa Mohammed Gamal Eldin <alaagamalel-din@hotmail.com>
2024-03-24 17:19:01 +0200

    trancform interface to class

--- src/Model/user.ts
+++ src/Model/user.ts
@@ -1,13 +1,24 @@
 import { Visibility } from "src/app/enums/friendsVidibility.enum";
-export interface User{
-    userId: string,
-    username: string,
-    userEmail: string,
-    firstName: string,
-    middleName: string,
-    lastName: string,
-    userTitle: string,
-    userAvatarUrl: string,
-    friendsVisibility: Visibility,
+export class User{
+    constructor(userId: string, username: string, userEmail: string, firstName: string, middleName: string, lastName: string, userTitle: string, userAvatarUrl: string, friendsVisibility: Visibility){
+        this.userId = userId;
+        this.username = username;
+        this.userEmail = userEmail;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.userTitle = userTitle;
+        this.userAvatarUrl = userAvatarUrl;
+        this.friendsVisibility = friendsVisibility;
+    }
+    userId: string;
+    username: string;
+    userEmail: string;
+    firstName: string;
+    middleName: string;
+    lastName: string;
+    userTitle: string;
+    userAvatarUrl: string;
+    friendsVisibility: Visibility;
    
 }
